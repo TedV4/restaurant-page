12e5575794505fe32ef1bb3f4d890c451c17dbdb
TedV4 <tedverdonkschot@outlook.com>
2022-12-31 16:21:34 -0700

    Add header to dom

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import "./style.css";
 
-const mainContainer = document.querySelector("content");
+const mainContainer = document.querySelector(".content");
 
 //Create header
 const header = document.createElement("header");
 const nav = document.createElement("nav");
 const aboutBtn = document.createElement("button");
+aboutBtn.textContent = "About";
 const menuBtn = document.createElement("button");
+menuBtn.textContent = "Menu";
 const contactBtn = document.createElement("button");
+contactBtn.textContent = "Contact";
 
-nav.append(aboutBtn, menuBtn, contactBtn);
+nav.appendChild(aboutBtn);
+nav.appendChild(menuBtn);
+nav.appendChild(contactBtn);
 header.appendChild(nav);
-mainContainer.append(header);
-
+mainContainer.appendChild(header);
