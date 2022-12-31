5f35eaeb3069446f3edce666b0a8dc390298b1e9
TedV4 <tedverdonkschot@outlook.com>
2022-12-31 16:07:55 -0700

    Update configurations

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('testing');
+import "./style.css";
+
+const mainContainer = document.querySelector("content");
+
+//Create header
+const header = document.createElement("header");
+const nav = document.createElement("nav");
+const aboutBtn = document.createElement("button");
+const menuBtn = document.createElement("button");
+const contactBtn = document.createElement("button");
+
+nav.append(aboutBtn, menuBtn, contactBtn);
+header.appendChild(nav);
+mainContainer.append(header);
+
